470923d6eb64f01e96f35b0a1a639c8eb761bf13
sharif.ahmed <sharif@commlinkinfotech.com.bd>
2023-05-30 08:48:54 +0600

    about page full complete

--- js/main.js
+++ js/main.js
@@ -4,8 +4,56 @@ $(document).ready(function () {
       nextEl: ".swiper-button-next",
       prevEl: ".swiper-button-prev",
     },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    slidesPerView: 5,
+    spaceBetween: 3,
+    freeMode: true,
+
   });
 
-  // 
-  
+  // var swiper = new Swiper(".mySwiper", {
+  //   pagination: {
+  //     el: ".swiper-pagination",
+  //   },
+  // });
+
+  var testimonialSwiper = new Swiper(".mySwiper-testimonial", {
+
+    pagination: {
+      el: ".swiper-pagination",
+
+    },
+
+  });
+
+  $('.skillbar').skillbar();
+
+  const ctx = document.getElementById('myChart');
+
+  new Chart(ctx, {
+    type: 'line',
+    data: {
+      labels: ['00', '01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11'],
+      datasets: [{
+        label: '# of Votes',
+        data: [12, 19, 33, 75, 62, 83, 95, 37, 29, 63, 92, 74, 85, 46, 78,],
+        borderWidth: 1
+      }]
+    },
+    options: {
+      scales: {
+        y: {
+          beginAtZero: true
+        }
+      }
+    }
+  });
+
+
+
+
+
 });
