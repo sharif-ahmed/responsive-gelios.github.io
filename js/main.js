5eb7c8a3b219bfee1d949f8cb3458f8b028b695b
sharif.ahmed <sharif@commlinkinfotech.com.bd>
2023-05-31 16:07:12 +0600

    portfolio filter js added

--- js/main.js
+++ js/main.js
@@ -1,23 +1,19 @@
 $(document).ready(function () {
-
-
-
-
   //********* ISOTOPE JS ************
-  // init Isotope
-  var $grid = $('.grid').isotope({
+  var $grid = $(".filter-container").isotope({
     // options
   });
   // filter items on button click
-  $('.filter-button-group').on('click', 'li', function () {
-    var filterValue = $(this).attr('data-filter');
+  $(".filter-menu").on("click", "li", function () {
+    var filterValue = $(this).attr("data-filter");
     $grid.isotope({ filter: filterValue });
   });
+
+  // filter items on active click
+  $(".filter-menu").on("click", "li", function () {
+    $(this).activeClass("active").sibilings.removeClass("active");
+  });
   //********* END ISOTOPE JS ************
-
-
-
-
 
   //********* SWIPER JS ************
 
@@ -28,16 +24,8 @@ $(document).ready(function () {
     },
     pagination: {
       el: ".swiper-pagination",
-
     },
-
   });
-
-  // var swiper = new Swiper(".mySwiper", {
-  //   pagination: {
-  //     el: ".swiper-pagination",
-  //   },
-  // });
 
   var testimonialSwiper = new Swiper(".mySwiper-testimonial", {
     pagination: {
@@ -50,36 +38,44 @@ $(document).ready(function () {
   });
   //********* END SWOPER JS ************
 
-
-
   //********** SKILL BAR JS *************
-  $('.skillbar').skillbar();
+  $(".skillbar").skillbar();
   //********** END SKILL BAR JS *************
 
-
-  
-
-
   //***************** CHART JS ****************
-  const ctx = document.getElementById('myChart');
+  const ctx = document.getElementById("myChart");
   new Chart(ctx, {
-    type: 'line',
+    type: "line",
     data: {
-      labels: ['00', '01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11'],
-      datasets: [{
-        label: '# of Votes',
-        data: [12, 19, 33, 75, 62, 83, 95, 37, 29, 63, 92, 74, 85, 46, 78,],
-        borderWidth: 1
-      }]
+      labels: [
+        "00",
+        "01",
+        "02",
+        "03",
+        "04",
+        "05",
+        "06",
+        "07",
+        "08",
+        "09",
+        "10",
+        "11",
+      ],
+      datasets: [
+        {
+          label: "# of Votes",
+          data: [12, 19, 33, 75, 62, 83, 95, 37, 29, 63, 92, 74, 85, 46, 78],
+          borderWidth: 1,
+        },
+      ],
     },
     options: {
       scales: {
         y: {
-          beginAtZero: true
-        }
-      }
-    }
+          beginAtZero: true,
+        },
+      },
+    },
   });
   //***************** END CHART JS ****************
-
 });
