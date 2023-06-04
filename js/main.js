6d825592520d22ca807b6e42c51f2ab79171831f
sharif.ahmed <sharif@commlinkinfotech.com.bd>
2023-06-04 08:43:45 +0600

    responsive css add some page

--- js/main.js
+++ js/main.js
@@ -1,46 +1,4 @@
 $(document).ready(function () {
-  //********* ISOTOPE JS ************
-  var $grid = $(".filter-container").isotope({
-    // options
-  });
-  // filter items on button click
-  $(".filter-menu").on("click", "li", function () {
-    var filterValue = $(this).attr("data-filter");
-    $grid.isotope({ filter: filterValue });
-  });
-
-  // filter items on active click
-  $(".filter-menu").on("click", "li", function () {
-    $(this).activeClass("active").sibilings.removeClass("active");
-  });
-  //********* END ISOTOPE JS ************
-
-  //********* SWIPER JS ************
-
-  var swiper = new Swiper(".mySwiper", {
-    navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
-    },
-    pagination: {
-      el: ".swiper-pagination",
-    },
-  });
-
-  var testimonialSwiper = new Swiper(".mySwiper-testimonial", {
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
-    },
-    slidesPerView: 5,
-    spaceBetween: 3,
-    freeMode: true,
-  });
-  //********* END SWOPER JS ************
-
-  //********** SKILL BAR JS *************
-  $(".skillbar").skillbar();
-  //********** END SKILL BAR JS *************
 
   //***************** CHART JS ****************
   const ctx = document.getElementById("myChart");
@@ -78,4 +36,82 @@ $(document).ready(function () {
     },
   });
   //***************** END CHART JS ****************
+
+
+  //********* SWIPER JS ************
+
+  var testimonialSwiper = new Swiper(".mySwiper-testimonial", {
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    slidesPerView: 5,
+    spaceBetween: 3,
+    freeMode: true,
+    breakpoints: {
+      480: {
+        slidesPerView: 1,
+        spaceBetween: 40,
+      },
+      640: {
+        slidesPerView: 2,
+        spaceBetween: 50,
+      }
+    }
+  });
+
+  var swiper = new Swiper(".mySwiper", {
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    slidesPerView: 1,
+    pagination: {
+      el: ".swiper-pagination",
+    },
+    breakpoints: {
+      480: {
+        slidesPerView: 1,
+        spaceBetween: 40,
+      },
+      640: {
+        slidesPerView: 1,
+        spaceBetween: 50,
+      }
+    }
+
+  });
+
+  //********* END SWOPER JS ************
+
+  // MOBILE MEAN MENU JS
+  jQuery(".mobile-menu").meanmenu({
+    meanScreenWidth: "767",
+    meanMenuContainer: ".mobile-menu-container"
+  });
+  //********* ISOTOPE JS ************
+  var $grid = $(".filter-container").isotope({
+    // options
+  });
+  // filter items on button click
+  $(".filter-menu").on("click", "li", function () {
+    var filterValue = $(this).attr("data-filter");
+    $grid.isotope({ filter: filterValue });
+  });
+
+  // filter items on active click
+  $(".filter-menu").on("click", "li", function () {
+    $(this).activeClass("active").sibilings.removeClass("active");
+  });
+  //********* END ISOTOPE JS ************
+
+
+  //********** SKILL BAR JS *************
+  $(".skillbar").skillbar();
+  //********** END SKILL BAR JS *************
+
+  
+
+
+
 });
